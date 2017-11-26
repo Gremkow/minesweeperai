f4ec88acc47316ef4a1ff8615808ff3534ff8987
Maurice Ackel <maurice.ackel@integra-ev.de>
2017-11-26 21:17:35 +0100

    Final

--- src/de/unima/info/ki/minesweeper/api/SatSolver.java
+++ src/de/unima/info/ki/minesweeper/api/SatSolver.java
@@ -1,18 +1,36 @@
 package de.unima.info.ki.minesweeper.api;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.sat4j.core.Vec;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.IConstr;
 import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVec;
+import org.sat4j.specs.IVecInt;
 import org.sat4j.specs.TimeoutException;
 
 public class SatSolver {
   ISolver solver;
+  IVec<IVecInt> clauses;
   
   public SatSolver() {
-     
+	  clauses = new Vec<>();
+	  solver = SolverFactory.newDefault();
+  }
+  
+  public void addClause(int[] clause) {
+	//int[] cl = clause.stream().mapToInt(Integer::intValue).toArray();
+	try {
+		solver.addClause(new VecInt(clause));
+	} catch (ContradictionException e) {
+		//Bad
+		e.printStackTrace();
+	}
   }
   
   /**
@@ -21,35 +39,30 @@ public class SatSolver {
    * @param variable
    * @return True, if the variable is a mine. False, if no reasoning can be done
    */
-  public boolean isMine(ArrayList<ArrayList<Integer>> KB, int variable) {
-    solver = SolverFactory.newDefault();
-    
-    //Add all clauses from the KB to the solver
-    for (ArrayList<Integer> clause : KB) {
-      int[] cl = clause.stream().mapToInt(Integer::intValue).toArray();
-      try {
-        solver.addClause(new VecInt(cl));
-      } catch (ContradictionException e) {
-        // An error here is bad, because this means the KB is not good
-        e.printStackTrace();
-      }
-    }
-    
-    //Add search variable to the solver
-    try {
-      solver.addClause(new VecInt(new int[] {-variable}));
-    } catch (ContradictionException e) {
-      // An error here is good, because this means that the variable leads to a contradiction
-      return true;
-    }
-    
-    try {
-      return !solver.isSatisfiable();
-    } catch (TimeoutException e) {
-      //If it takes too long, simply say, that no knowledge was created
-      e.printStackTrace();
-      return false;
-    }
+  public boolean isMine(int variable) {
+	IConstr constraint = null;
+	  
+	//Add search variable to the solver
+	try {
+	  constraint = solver.addClause(new VecInt(new int[] {-variable}));
+	  if(!solver.isSatisfiable()){
+		if(constraint != null) {
+		  solver.removeConstr(constraint);
+		}
+		return true;
+	  } else {
+		if(constraint != null) {
+	      solver.removeConstr(constraint);
+		}
+		return false;
+	  }
+	} catch (ContradictionException e) {
+	  // An error here is good, because this means that the variable leads to a contradiction
+	  return true;
+	} catch (TimeoutException e) {
+	  e.printStackTrace();
+	  return false;
+	}
   }
   
   /**
@@ -58,35 +71,30 @@ public class SatSolver {
    * @param variable
    * @return True, if the variable is not a mine. False, if no reasoning can be done
    */
-  public boolean isNoMine(ArrayList<ArrayList<Integer>> KB, int variable) {
-    solver = SolverFactory.newDefault();
-    
-    //Add all clauses from the KB to the solver
-    for (ArrayList<Integer> clause : KB) {
-      int[] cl = clause.stream().mapToInt(Integer::intValue).toArray();
-      try {
-        solver.addClause(new VecInt(cl));
-      } catch (ContradictionException e) {
-        // An error here is bad, because this means the KB is not good
-        e.printStackTrace();
-      }
-    }
-    
+  public boolean isNoMine(int variable) {
+	IConstr constraint = null;
+	  
     //Add search variable to the solver
     try {
-      solver.addClause(new VecInt(new int[] {variable}));
+      constraint = solver.addClause(new VecInt(new int[] {variable}));
+      if(!solver.isSatisfiable()){
+    	if(constraint != null) {
+    	  solver.removeConstr(constraint);
+    	}
+    	return true;
+      } else {
+    	if(constraint != null) {
+          solver.removeConstr(constraint);
+    	}
+    	return false;
+      }
     } catch (ContradictionException e) {
       // An error here is good, because this means that the variable leads to a contradiction
       return true;
-    }
-    
-    try {
-      return !solver.isSatisfiable();
     } catch (TimeoutException e) {
-      //If it takes too long, simply say, that no knowledge was created
-      e.printStackTrace();
-      return false;
-    }
+	  e.printStackTrace();
+	  return false;
+	}
   }
 
 }
